9cf4c5d3c8ed26c900c2d16cdf97d4d80cac0ee1
99Alphapriority <prajneets@gmail.com>
2024-03-10 13:51:26 -0600

    history feature implemented

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/wait.h>
+#include<assert.h>
 
 #define MAX_LINE 80 /*The maximum length command*/
 #define BUFF_SIZE 32
@@ -22,8 +23,9 @@ int check_ampersand(char** args)
 		if((strcmp(args[i], "&") == 0) && (args[i+1] == NULL))
 		{
 			args[i] = NULL;
-			return 1;
+			return 0;
 		}
+		i++;
 	}
 
 	/*check if "&" is appended to the last command in the line
@@ -35,12 +37,12 @@ int check_ampersand(char** args)
 		if('&' == args[i][j])
 		{
 			args[i][j] = '\0';
-			return 1;
+			return 0;
 		}
 		j++;
 	}
 
-	return 0;
+	return 1;
 }
 /******************************************************************************
  * Execute the command using child process
@@ -132,9 +134,14 @@ int main(int argc, char **argv)
 	char *args[MAX_LINE/2 + 1]; /*command line arguments*/
 	int should_run = 1; /*flag to determine when to exit program*/
 	char* input;
+	FILE* fp = fopen("history.txt", "w");
+	assert(fp != NULL);
+	char *lastCmd[MAX_LINE/2 + 1]; /*to store last executed command*/
 
 	while(should_run)
 	{
+		int idx = 0;
+
 		printf("osh> ");
 		fflush(stdout);
 
@@ -143,35 +150,87 @@ int main(int argc, char **argv)
 
 		if(NULL == args[0])
 			continue;
-		else if(strcmp(args[0], "exit") == 0)
-			should_run = 0;
-		else if(strcmp(args[0], "cd")== 0)
+
+		if(strcmp(args[0], "!!") == 0)
 		{
-			if(args[1] == NULL)
+			if(NULL == lastCmd[0])
 			{
-				fprintf(stderr,"osh: expected arguments to \"cd\"\n");
-				return 1;	
+				fprintf(stderr,"osh: No commands in history\n");
+				continue;
 			}
-			else if(0 != chdir(args[1]))
-			{
-				perror("osh");
-				return 1;
-			}
+
+			if(strcmp(lastCmd[0], "cd")== 0)
+                	{
+                        	if(lastCmd[1] == NULL)
+                        	{
+                                	fprintf(stderr,"osh: expected arguments to \"cd\"\n");
+                                	return 1;
+                        	}
+                        	else if(0 != chdir(lastCmd[1]))
+                        	{
+                                	perror("osh");
+                                	return 1;
+                        	}
+                	}
+                	else
+                	{		
+                        	/**
+                        	* After reading user input, the steps are:
+                        	* (1) fork a child process using fork()
+                        	* (2) the child process will invoke execvp()
+                        	* (3) parent will invoke wait() unless command included &
+                        	*/
+
+                        	int should_wait = check_ampersand(lastCmd);
+                        	execute_cmd(lastCmd, should_wait);
+                	}
 		}
 		else
 		{
-			/**
-                 	* After reading user input, the steps are:
-                 	* (1) fork a child process using fork()
-                 	* (2) the child process will invoke execvp()
-                 	* (3) parent will invoke wait() unless command included &
-                 	*/
-			
-			int should_wait = check_ampersand(args);
-			execute_cmd(args, should_wait);
-		}
+			/*write the command in the history*/
+			fprintf(fp,"%s\n", input);
+		
+			/*copy the command in lastCmd*/
+			memset(lastCmd, 0, sizeof(lastCmd));
+			while(NULL != args[idx])
+			{
+				lastCmd[idx] = args[idx];
+				idx++;
+			};
+			lastCmd[idx] = NULL;
 
+
+			if(strcmp(args[0], "exit") == 0)
+				should_run = 0;
+			else if(strcmp(args[0], "cd")== 0)
+			{
+				if(args[1] == NULL)
+				{
+					fprintf(stderr,"osh: expected arguments to \"cd\"\n");
+					return 1;	
+				}
+				else if(0 != chdir(args[1]))
+				{
+					perror("osh");
+					return 1;
+				}
+			}
+			else
+			{
+				/**
+                 		* After reading user input, the steps are:
+                 		* (1) fork a child process using fork()
+                 		* (2) the child process will invoke execvp()
+                 		* (3) parent will invoke wait() unless command included &
+                 		*/
+			
+				int should_wait = check_ampersand(args);
+				execute_cmd(args, should_wait);
+			}
+
+		}
 	}
 
+	fclose(fp);
 	return 0;
 }
